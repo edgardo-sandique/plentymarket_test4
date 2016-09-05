57cc5f46a5bd2d9a6cf172bbb4c8116e4c652c37
Christoph Harms-Ensink <christoph.harms-ensink@plentymarkets.com>
2016-09-05 17:11:20 +0200

    UPDATE REST Doku

--- src/Controllers/RestController.hh
+++ src/Controllers/RestController.hh
@@ -11,6 +11,20 @@ class RestController extends Controller
 	 */
 	public function showRestModule(Twig $twig, string $moduleName):string
 	{
+		return $twig->render('PlentyPluginShowcase::rest.intermediate_'.$moduleName);
+	}
+
+	/**
+	*
+	*/
+	public function showRestModuleDetail(Twig $twig, string $moduleName):string
+	{
 		return $twig->render('PlentyPluginShowcase::rest.'.$moduleName);
 	}
+
+	public function showRestIntroduction(Twig $twig):string
+	{
+		return $twig->render('PlentyPluginShowcase::rest.introduction');
+	}
+
 }
--- src/Providers/ShowcaseRestRouteServiceProvider.hh
+++ src/Providers/ShowcaseRestRouteServiceProvider.hh
@@ -18,7 +18,14 @@ class ShowcaseRestRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router):void
 	{
+
+		$router->get('rest-doc/introduction', 'Showcase\Controllers\RestController@showRestIntroduction')
+			->where('moduleName', '[a-zA-Z_]+');
+
 		$router->get('rest-doc/{moduleName}', 'Showcase\Controllers\RestController@showRestModule')
+			->where('moduleName', '[a-zA-Z_]+');
+
+		$router->get('rest-doc/{moduleName}/details', 'Showcase\Controllers\RestController@showRestModuleDetail')
 			->where('moduleName', '[a-zA-Z_]+');
 	}
 }
